10a3891e9a86d2fe99e1f8fb7be4fd08314f2112
Jabez Villamarzo <simplesimmering@gmail.com>
2024-02-06 15:56:53 +0800

    chore(refactor): Add Game module with init_board fn

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const Game = (() => {
+	const board = [];
+	const empty_cell = "_";
+
+	function new_board() {
+		board.splice(0, board.length);
+
+		for (let i=0; i<3; i++) {
+			const row = [];
+			for (let j=0; j<3; j++) {
+				row.push(empty_cell);
+			}
+
+			board.push(row);
+		}
+
+		return board;
+	}
+
+	return {
+		board: new_board(),
+	};
+})();
+
 window.addEventListener("DOMContentLoaded", () => {
-	console.log("test succeeded");
+	console.log(Game.board);
 });
