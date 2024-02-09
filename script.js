857d9dae14a81758c75481639b127f85592cdd99
Jabez Villamarzo <simplesimmering@gmail.com>
2024-02-09 22:56:40 +0800

    feat(game): Add place mark function

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ const GameState = (() => {
 		},
 
 		set current_move(value) {
-			for (const valid_state in CELL_STATES) {
-				if (CELL_STATES[valid_state] === value) {
+			for (const valid_state of [CELL_STATES.o_mark, CELL_STATES.x_mark]) {
+				if (valid_state === value) {
 					move = value;
 					return;
 				}
@@ -28,7 +28,8 @@ const GameState = (() => {
 })();
 
 const Game = ((import_state) => {
-	let { board, CELL_STATES } = import_state;
+	const { board, CELL_STATES } = import_state;
+	const { empty_cell, x_mark, o_mark } = CELL_STATES;
 
 	function reset_board() {
 		board.splice(0, board.length);
@@ -36,7 +37,7 @@ const Game = ((import_state) => {
 		for (let i=0; i<3; i++) {
 			const row = [];
 			for (let j=0; j<3; j++) {
-				row.push(CELL_STATES.empty_cell);
+				row.push(empty_cell);
 			}
 
 			board.push(row);
@@ -44,12 +45,32 @@ const Game = ((import_state) => {
 	}
 
 	function new_game() {
-		import_state.current_move = CELL_STATES.o_mark;
+		import_state.current_move = o_mark;
 		reset_board();
+	}
+
+	function finish_turn() {
+		let new_state;
+
+		switch (import_state.current_move) {
+			case x_mark: new_state = o_mark;
+			case o_mark: new_state = x_mark;
+		}
+
+		import_state.current_move = new_state;
+		console.table(board);
+	}
+
+	function place_mark(move) {
+		const { x, y } = move;
+
+		board[x][y] = import_state.current_move;
+		finish_turn();
 	}
 
 	return {
 		new_game,
+		place_mark,
 	};
 })(GameState);
 
